2c19dad40a5bb1b751d76bc78fb935448c4bdcf0
oleksiiyudin <oleksejudin6@gmail.com>
2023-06-25 21:09:43 +0300

    add news

--- src/database/methods/jobs.py
+++ src/database/methods/jobs.py
@@ -1,7 +1,7 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm.attributes import instance_dict
 
-from crawlers.get_jobs import get_jobs
+
 from src.database.models import Jobs
 from uuid import UUID
 from sqlalchemy import select
