109c478e2f1d93cb9c343288a0b1a5c4c359ff92
oleksiiyudin <oleksejudin6@gmail.com>
2023-07-09 00:00:18 +0300

    fix cors

--- src/database/models.py
+++ src/database/models.py
@@ -9,7 +9,7 @@ Base = declarative_base()
 class User(Base):
     __tablename__= "user"
 
-    user_id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4())
+    user_id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     name = Column(String, nullable=False)
     surname = Column(String, nullable=False)
     email = Column(String, nullable=False, unique=True)
